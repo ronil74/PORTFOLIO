3ac4da0e94c8dc79db3a4b20b0d64a083c1fdede
ronil74 <ronilrodrigues74@gmail.com>
2023-08-19 15:30:03 +0530

    Test branch flask

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
 from flask import Flask
 app = Flask(__name__)
 
-  
+
+print(__name__)
+   
 
 @app.route('/')
 def Fun():
-    return 'Hello world'
+    return 'Hello world,m'
+
+@app.route('/about')
+def about():
+    return 'Runner'
 
 
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=3000)
+# if __name__ == "__main__":
+#     app.run(debug=True, host="127.0.0.1", port=5000)
    
