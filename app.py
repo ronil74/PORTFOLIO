18c78ed12096f1b228d20148f604749204642259
ronil74 <ronilrodrigues74@gmail.com>
2023-08-28 15:30:21 +0530

    Thank you page included

--- app.py
+++ app.py
@@ -21,6 +21,11 @@ def contact():
 def work():
     return render_template('work.html')
 
+@app.route('/submit_form', methods = ['GET','POST'])
+def form():
+    message="Form Submitted, We will get in touch to you shortly!!"
+    return render_template('thankyou.html',message=message)
+
 # if __name__ == "__main__":
 #     app.run(debug=True, host="127.0.0.1", port=5000)
    
