89a4387b39798bdbb964d0027934204befeea8cc
ronil74 <ronilrodrigues74@gmail.com>
2023-08-28 18:17:35 +0530

    csv included

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask,render_template,url_for
+from flask import Flask,render_template,url_for,redirect,request
+import csv
 app = Flask(__name__)
 
 
@@ -23,8 +24,26 @@ def work():
 
 @app.route('/submit_form', methods = ['GET','POST'])
 def form():
-    message="Form Submitted, We will get in touch to you shortly!!"
-    return render_template('thankyou.html',message=message)
+    if request.method == "POST":
+        try:
+            data = request.form.to_dict()
+            write_data_csv(data)
+            message = 'Form Submitted, We will get in touch to you shortly!!'
+            return render_template('thankyou.html',message=message)
+        except:
+            message = "DID NOT SAVE DATA TO DATABASE."
+            return render_template('thankyou.html',message=message)
+    else:
+        message = "FORM NOT SUBMITTED"
+        return render_template('thankyou.html',message=message)
+    
+def write_data_csv(data):
+    email = data['email']
+    subject = data['subject']
+    message = data["message"]
+    with open('db.csv', 'a', newline='') as csvfile:
+        db_writer = csv.writer(csvfile, delimiter=',',quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        db_writer.writerow([email,subject,message])
 
 # if __name__ == "__main__":
 #     app.run(debug=True, host="127.0.0.1", port=5000)
