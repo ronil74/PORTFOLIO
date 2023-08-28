4f4c465db3322ee0a68afb51e2fa184b6191f221
ronil74 <ronilrodrigues74@gmail.com>
2023-08-28 12:41:20 +0530

    new - reevrted changes

--- app.py
+++ app.py
@@ -1,13 +1,26 @@
-from flask import Flask
+from flask import Flask,render_template,url_for
 app = Flask(__name__)
 
-  
+
+print(__name__)
+   
 
 @app.route('/')
-def Fun():
-    return 'Hello world'
+def index():
+    return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=3000)
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/work')
+def work():
+    return render_template('work.html')
+
+# if __name__ == "__main__":
+#     app.run(debug=True, host="127.0.0.1", port=5000)
    
